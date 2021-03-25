f6b939d099dadec464d092c6a2bf9be877f6929a
Andrei Campean <andreicampean@gmail.com>
2021-03-25 16:10:59 +0200

    inlocuire chartjs, preluare date 3 tabele, 3 sectiuni linii

--- nord_manage/wave/views.py
+++ nord_manage/wave/views.py
@@ -4,13 +4,39 @@ from django.db import connection
 
 
 def home(request):
-    cursor = connection.cursor()
-    cursor.execute('''select cod_placa, count(*) as nr_buc
+    linia1 = connection.cursor()
+    linia1.execute('''select cod_placa, count(*) as nr_buc
         from (select wave_date_placi.cod_placa, wave_date_placi.min_placa, wave_productie.data, wave_productie.line_productie
       from wave_productie
                inner join wave_date_placi on wave_productie.cod_placa_id = wave_date_placi.id
       where CAST(wave_productie.data as Date) = CAST(NOW() as Date)
         and wave_productie.line_productie = 1) as wpwdp
         group by cod_placa;''')
-    results = cursor.fetchall()
-    return render(request, 'wave/home.html', {'data': results})
+    results_linia1 = linia1.fetchall()
+
+    linia2 = connection.cursor()
+    linia2.execute('''select cod_placa, count(*) as nr_buc
+        from (select wave_date_placi.cod_placa, wave_date_placi.min_placa, wave_productie.data, wave_productie.line_productie
+      from wave_productie
+               inner join wave_date_placi on wave_productie.cod_placa_id = wave_date_placi.id
+      where CAST(wave_productie.data as Date) = CAST(NOW() as Date)
+        and wave_productie.line_productie = 2) as wpwdp
+        group by cod_placa;''')
+    results_linia2 = linia2.fetchall()
+
+    linia3 = connection.cursor()
+    linia3.execute('''select cod_placa, count(*) as nr_buc
+        from (select wave_date_placi.cod_placa, wave_date_placi.min_placa, wave_productie.data, wave_productie.line_productie
+      from wave_productie
+               inner join wave_date_placi on wave_productie.cod_placa_id = wave_date_placi.id
+      where CAST(wave_productie.data as Date) = CAST(NOW() as Date)
+        and wave_productie.line_productie = 3) as wpwdp
+        group by cod_placa;''')
+    results_linia3 = linia3.fetchall()
+
+    context = {
+      'linia1': results_linia1,
+      'linia2': results_linia2,
+      'linia3': results_linia3,
+    }
+    return render(request, 'wave/home.html', context)
