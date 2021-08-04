181e8db3eb8ef7f4c8c18ef90ece981089579054
Andrei Campean <andreicampean@gmail.com>
2021-08-04 12:06:35 +0300

    editare rand fara reload defecte
    - editare rand si trimitere POST data la backend fara reload;
    - inchidere modal dupa ce se apasa pe ok.

--- nord_manage/defecte/urls.py
+++ nord_manage/defecte/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='defecte-home'),
     path('import/', views.import_defecte, name='defecte-import' ),
     #path('import/upload/', views.upload_file, name='defecte-upload' ),
+    #path('import/update/', views.update, name='defecte-update')
 ]
