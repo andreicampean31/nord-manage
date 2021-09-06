1a34f8497d54832e7c4746c731487ff79b6d458f
Andrei Campean <andreicampean@gmail.com>
2021-09-06 23:08:00 +0300

    adaugare/update alarme
    - adaugare ore noi
    - update date alarme existente

--- nord_manage/sonerie/models.py
+++ nord_manage/sonerie/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 
 
-class Sonerii(models.Model):
+class Info_Sonerii(models.Model):
     denumire = models.CharField(max_length=200)
     pin = models.CharField(max_length=200)
     versiune = models.CharField(max_length=200)
-    ore = models.CharField(max_length=200)
+    status = models.BooleanField()
+    
+class Ore_Sonerii(models.Model):
+    soneria = models.ForeignKey(Info_Sonerii, on_delete=models.CASCADE)
+    ora = models.TimeField()
     status = models.BooleanField()
 # Create your models here.
--- nord_manage/sonerie/views.py
+++ nord_manage/sonerie/views.py
@@ -1,17 +1,18 @@
+
 from django.db import connections
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from .models import Sonerii
+from .models import Info_Sonerii, Ore_Sonerii
+from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
-    lista_sonerii = Sonerii.objects.values('denumire', 'status')
+    lista_sonerii = Info_Sonerii.objects.values('id', 'denumire', 'status')
 
     
     sonerii = [None]*len(lista_sonerii)
     status = [None]*len(lista_sonerii)
     j=0
     for i in lista_sonerii:
-        print(i['denumire'])
         sonerii[j] = i['denumire']
         status[j] = i['status']
         j += 1
@@ -23,24 +24,86 @@ def home(request):
     return render(request, 'sonerie/home.html', context)
     #return JsonResponse({'status': 202})
 
+@csrf_exempt
 def getSettings(request):
     if request.method == 'GET':
         denumire = request.GET.get('sonerie')
         
-        setari = Sonerii.objects.filter(denumire = denumire)
-        print(setari[0].versiune)
+        setari = Info_Sonerii.objects.filter(denumire = denumire)
+        ore = Ore_Sonerii.objects.filter(soneria_id__denumire = denumire)
         
-        ore = setari[0].ore
+        lista_ore = []
+        status = []
+        
+        for i in ore:
+            lista_ore.append(i.ora)
+            status.append(i.status)
+            
+        #ore = setari[0].ore
     return JsonResponse({
-        'status': 202,
+        'id': setari[0].id,
         'denumire': setari[0].denumire,
         'pin': setari[0].pin,
         'versiune': setari[0].versiune,
-        'ore': ore.split(","),
-        'status': setari[0].status
+        'ore': lista_ore,
+        'status': status
         })
-# Create your views here.
 
+@csrf_exempt
+def updateSettings(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        denumire = request.POST.get('denumire')
+        pin = request.POST.get('pin')
+        ore_active = request.POST.getlist('alarme_active[]')
+        ore_inactive = request.POST.getlist('alarme_inactive[]')
+        #ore_noi = request.POST.getlist('alarme_noi[]')
+        
+        Info_Sonerii.objects.filter(id = id).update(denumire = denumire, pin = pin)
+        for i in ore_active:
+            k=0
+            for j in ore_active:
+                if i==j:
+                    k += 1
+            for j in ore_inactive:
+                if i==j:
+                    k += 1
+            if k > 1:
+                return JsonResponse({'status': 404, 'desc': 'duplicate'})
+            
+        for i in ore_inactive:
+            k=0
+            for j in ore_active:
+                if i==j:
+                    k += 1
+            for j in ore_inactive:
+                if i==j:
+                    k += 1
+            if k > 1:
+                return JsonResponse({'status': 404, 'desc': 'duplicate'})
+        
+        for i in ore_active:
+            ora = Ore_Sonerii.objects.filter(soneria_id = id, ora = i)
+            if ora:
+                if ora[0].status == False:
+                    Ore_Sonerii.objects.filter(soneria_id = id, ora = i).update(status = True)
+            else:
+                alarma_noua = Ore_Sonerii(soneria_id = id, ora = i, status = True)
+                alarma_noua.save()
+            
+        
+        for i in ore_inactive:
+            ora = Ore_Sonerii.objects.filter(soneria_id = id, ora = i)
+            if ora:
+                if ora[0].status == True:
+                    Ore_Sonerii.objects.filter(soneria_id = id, ora = i).update(status = False)
+            else:
+                alarma_noua = Ore_Sonerii(soneria_id = id, ora = i, status = False)
+                alarma_noua.save()
+            
+
+        
+    return JsonResponse({'stauts': 202})
 def ardu(request):
     #return HttpResponse(status = 202)
     
