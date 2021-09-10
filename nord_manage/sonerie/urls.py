756f8fcdef64c4d827c5c9acb55533494a4b4235
Andrei Campean <andreicampean@gmail.com>
2021-09-11 00:14:00 +0300

    dezactivare activare alarme
    - dezactivare si stergere alarme cand sunt dezactivate toate orele
    - activare alarma cand cel putin o ora este activa

--- nord_manage/sonerie/urls.py
+++ nord_manage/sonerie/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='sonerie-home'),
+    path('getAlarms', views.getAlarms, name='sonerie-getAlarms'),
     path('getSettings/', views.getSettings, name='sonerie-getsettings'),
     path('updateSettings/', views.updateSettings, name='sonerie-updateSettings'),
     path('arduGetSettings/', views.ardu_get_settings, name='sonerie-arduGetSettings')
--- nord_manage/sonerie/views.py
+++ nord_manage/sonerie/views.py
@@ -1,10 +1,11 @@
-
 from decimal import InvalidOperation
+#from nord_manage import sonerie
 from django.db import connections
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from .models import Info_Sonerii, Ore_Sonerii
 from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
 
 def home(request):
     lista_sonerii = Info_Sonerii.objects.values('id', 'denumire', 'status')
@@ -25,6 +26,21 @@ def home(request):
     return render(request, 'sonerie/home.html', context)
     #return JsonResponse({'status': 202})
 
+def getAlarms(request):
+    if request.method == 'GET':
+        sonerii = Info_Sonerii.objects.values('denumire', 'status')
+        
+        denumiri = []
+        status = []
+        for i in sonerii:
+            denumiri.append(i['denumire'])
+            status.append(i['status'])
+            
+    return JsonResponse({
+        'denumiri': denumiri,
+        'status': status
+    })
+    
 @csrf_exempt
 def getSettings(request):
     if request.method == 'GET':
@@ -58,6 +74,8 @@ def updateSettings(request):
         pin = request.POST.get('pin')
         ore_active = request.POST.getlist('alarme_active[]')
         ore_inactive = request.POST.getlist('alarme_inactive[]')
+        
+        
         #ore_noi = request.POST.getlist('alarme_noi[]')
         
         version = Info_Sonerii.objects.values('versiune')
@@ -107,36 +125,51 @@ def updateSettings(request):
             else:
                 alarma_noua = Ore_Sonerii(soneria_id = id, ora = i, status = False)
                 alarma_noua.save()
+                
+        if ore_active == []:
+            Info_Sonerii.objects.filter(denumire = denumire).update(status = False)
+            for i in ore_inactive:
+                time = datetime.strptime(i+':00', '%H:%M:%S')
+                Ore_Sonerii.objects.filter(soneria_id = id, ora = time).delete()
+        else:
+            Info_Sonerii.objects.filter(denumire = denumire).update(status = True)
+            
+        if ore_active != []:
+            Info_Sonerii.objects.filter(denumire = denumire).update(status = True)
             
 
         
     return JsonResponse({'stauts': 202})
 def ardu_get_settings(request):
-    
-    setari = Info_Sonerii.objects.all()
-    #ore = Ore_Sonerii.objects.all()
-        
-    setari_json = {
-        'status': 202,
-        'names': [None]*len(setari),
-        'pins': [None]*len(setari),
-        'versiune': setari[0].versiune,
-        'status': [None]*len(setari),
-        'ore': [None]*len(setari)
-    }
-    
-    for i in range(0, len(setari)):
-        ore = Ore_Sonerii.objects.filter(soneria_id__denumire = setari[i].denumire)
-        
-        ore_str = ''
-        for j in ore:
-            ore_str += str(j.ora) + ','
+    if request.method == 'GET':
+        setari = Info_Sonerii.objects.all()
+        #ore = Ore_Sonerii.objects.all()
             
-        print(ore_str)
-        #print(ore)
-        setari_json['names'][i] = setari[i].denumire
-        setari_json['pins'][i] = setari[i].pin
-        setari_json['status'][i] = setari[i].status
-        setari_json['ore'][i] = ore_str
+        setari_json = {
+            'status': 202,
+            'names': [None]*len(setari),
+            'pins': [None]*len(setari),
+            'versiune': setari[0].versiune,
+            'status': [None]*len(setari),
+            'ore': [None]*len(setari)
+        }
+        
+        for i in range(0, len(setari)):
+            ore = Ore_Sonerii.objects.filter(soneria_id__denumire = setari[i].denumire, status = True)
+            
+            ore_list = ''
+            for j in range(0, len(ore)):
+                if j < len(ore)-1:
+                    ore_list += str(ore[j].ora) + ','
+                    
+                else:
+                    ore_list += str(ore[j].ora) + '.'
+                
+            #print(ore_str)
+            #print(ore)
+            setari_json['names'][i] = setari[i].denumire
+            setari_json['pins'][i] = setari[i].pin
+            setari_json['status'][i] = setari[i].status
+            setari_json['ore'][i] = ore_list
         
     return JsonResponse(setari_json)
